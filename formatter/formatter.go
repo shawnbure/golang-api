225f315f71f3a3216141e457f0cfb048d0cbe411
Newname <new@email>
2007-05-19 01:01:01 -0700

    formatter wip

---
+++ formatter/formatter.go
@@ -0,0 +1,117 @@
+package formatter
+
+import (
+	"encoding/hex"
+	"errors"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/data"
+	"github.com/btcsuite/btcutil/bech32"
+	"github.com/erdsea/erdsea-api/config"
+	"math/big"
+	"strconv"
+)
+
+var (
+	listNftEndpointName         = "putNftForSale"
+	buyNftEndpointName          = "buyNft"
+	withdrawNftEndpointName     = "withdrawNft"
+	ESDTNFTTransferEndpointName = "ESDTNFTTransfer"
+)
+
+type TxFormatter struct {
+	config config.BlockchainConfig
+}
+
+func NewTxFormatter(cfg config.BlockchainConfig, ) TxFormatter {
+	return TxFormatter{config: cfg}
+}
+
+func (f *TxFormatter) NewListNftTxTemplate(senderAddr string, tokenId string, nonce uint64, price string) (*data.Transaction, error) {
+	address, err := data.NewAddressFromBech32String(senderAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	address1, address2, adddress3 := bech32.Decode(senderAddr)
+	Use(address1, address2, adddress3)
+
+	priceBigInt, success := big.NewInt(0).SetString(price, 10)
+	if !success {
+		return nil, errors.New("cannot parse price")
+	}
+
+	txData := ESDTNFTTransferEndpointName +
+		"@" + hex.EncodeToString([]byte(tokenId)) +
+		"@" + hex.EncodeToString(big.NewInt(int64(nonce)).Bytes()) +
+		"@" + hex.EncodeToString(big.NewInt(int64(1)).Bytes()) +
+		"@" + hex.EncodeToString(address.AddressBytes()) +
+		// How the fuck do I get Decoded Address as fucking hex?
+		// Maybe using bech32.Decode one untrained eye may think.
+		// Haha! You fool, you have to work harder than this.
+		"@" + hex.EncodeToString([]byte(listNftEndpointName)) +
+		"@" + hex.EncodeToString(priceBigInt.Bytes())
+
+	tx := data.Transaction{
+		Nonce:     0,
+		Value:     "0",
+		RcvAddr:   f.config.MarketplaceAddress,
+		SndAddr:   senderAddr,
+		GasPrice:  f.config.GasPrice,
+		GasLimit:  f.config.ListNftGasLimit,
+		Data:      []byte(txData),
+		Signature: "",
+		ChainID:   f.config.ChainID,
+		Version:   1,
+		Options:   0,
+	}
+
+	return &tx, nil
+}
+
+func (f *TxFormatter) NewBuyNftTxTemplate(senderAddr string, tokenId string, nonce uint64, price string) data.Transaction {
+	priceDec := big.NewInt(0).SetBytes([]byte(price)).Int64()
+	priceDecStr := strconv.FormatInt(priceDec, 10)
+
+	txData := buyNftEndpointName +
+		"@" + hex.EncodeToString([]byte(tokenId)) +
+		"@" + hex.EncodeToString(big.NewInt(int64(nonce)).Bytes())
+
+	return data.Transaction{
+		Nonce:     0,
+		Value:     priceDecStr,
+		RcvAddr:   f.config.MarketplaceAddress,
+		SndAddr:   senderAddr,
+		GasPrice:  f.config.GasPrice,
+		GasLimit:  f.config.BuyNftGasLimit,
+		Data:      []byte(txData),
+		Signature: "",
+		ChainID:   f.config.ChainID,
+		Version:   1,
+		Options:   0,
+	}
+}
+
+func (f *TxFormatter) NewWithdrawNftTxTemplate(senderAddr string, tokenId string, nonce uint64) data.Transaction {
+	txData := withdrawNftEndpointName +
+		"@" + hex.EncodeToString([]byte(tokenId)) +
+		"@" + hex.EncodeToString(big.NewInt(int64(nonce)).Bytes())
+
+	return data.Transaction{
+		Nonce:     0,
+		Value:     "0",
+		RcvAddr:   f.config.MarketplaceAddress,
+		SndAddr:   senderAddr,
+		GasPrice:  f.config.GasPrice,
+		GasLimit:  f.config.WithdrawNftGasLimit,
+		Data:      []byte(txData),
+		Signature: "",
+		ChainID:   f.config.ChainID,
+		Version:   1,
+		Options:   0,
+	}
+}
+
+func Use(vals ...interface{}) {
+	for _, val := range vals {
+		_ = val
+	}
+}
--- go.mod
+++ go.mod
@@ -6,17 +6,22 @@ require (
 	github.com/ElrondNetwork/elrond-go-core v1.0.1-0.20210721164025-65cf7f169349
 	github.com/ElrondNetwork/elrond-go-logger v1.0.4
 	github.com/ElrondNetwork/elrond-sdk-erdgo v1.0.0
+	github.com/btcsuite/btcutil v1.0.2
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v0.0.0-20190301062745-f9e10995c85a
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-redis/cache/v8 v8.4.2
 	github.com/go-redis/redis/v8 v8.11.3
 	github.com/lib/pq v1.6.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/uptrace/uptrace-go v1.0.1
 	github.com/urfave/cli v1.22.5
-	go.opentelemetry.io/otel/metric v0.22.0
 	go.uber.org/atomic v1.7.0
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.14
 )
