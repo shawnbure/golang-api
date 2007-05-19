d5e520b827225fb621309fec6bd2db4756e04e77
Newname <new@email>
2007-05-19 01:01:01 -0700

    mint tokens gas limit

--- formatter/txFormatter.go
+++ formatter/txFormatter.go
@@ -333,7 +333,7 @@ func (f *TxFormatter) NewMintNftsTxTemplate(
 	mintPricePerToken float64,
 	numberOfTokens uint64,
 ) Transaction {
-	gasLimit := f.config.MintTokenGasLimit * numberOfTokens
+	gasLimit := f.config.MintTokenGasLimit * (numberOfTokens / 10 + 1)
 	totalPrice := fmt.Sprintf("%f", mintPricePerToken*float64(numberOfTokens))
 	txData := mintTokensEndpointName +
 		"@" + hex.EncodeToString(big.NewInt(int64(numberOfTokens)).Bytes())
