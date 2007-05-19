74104c272b65b48664502bfcbbaecdeb31fdbb4b
Newname <new@email>
2007-05-19 01:01:01 -0700

    added basic write and read tests

---
+++ storage/connection_test.go
@@ -0,0 +1,52 @@
+package storage
+
+import (
+	"github.com/erdsea/erdsea-api/config"
+	"github.com/erdsea/erdsea-api/data"
+	_ "github.com/lib/pq"
+	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const ConfigTestFilePath = "../config/config_test.toml"
+
+func Test_Connection(t *testing.T) {
+	connectToDb(t)
+}
+
+func Test_BasicWrite(t *testing.T) {
+	connectToDb(t)
+
+	collection := defaultCollection()
+	tx := GetDB().Create(&collection)
+	require.Nil(t, tx.Error)
+}
+
+func Test_BasicWriteRead(t *testing.T) {
+	connectToDb(t)
+
+	collection := defaultCollection()
+	tx := GetDB().Create(&collection)
+	require.Nil(t, tx.Error)
+
+	var collectionRead data.Collection
+	txRead := GetDB().Last(&collectionRead)
+
+	require.Nil(t, txRead.Error)
+	assert.Equal(t, collectionRead, collection)
+}
+
+func connectToDb(t *testing.T) {
+	cfg, err := config.LoadConfig(ConfigTestFilePath)
+	require.Nil(t, err)
+	Connect(cfg.Database)
+}
+
+func defaultCollection() data.Collection {
+	return data.Collection{
+		Name:      "default",
+		TokenID:   "my_token",
+		CreatorID: 0,
+	}
+}
