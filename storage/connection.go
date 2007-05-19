650b2af88b1902097d11fcfce1f203f2c3802419
Newname <new@email>
2007-05-19 01:01:01 -0700

    sort rules

--- storage/connection.go
+++ storage/connection.go
@@ -44,8 +44,6 @@ func Connect(cfg config.DatabaseConfig) {
 		if err != nil {
 			panic(err)
 		}
-
-		InsertOrUpdateEntities()
 	})
 }
 
@@ -91,26 +89,6 @@ func TryMigrate() error {
 	}
 
 	return nil
-}
-
-func InsertOrUpdateEntities() {
-	collection := &entities.Collection{ID: 0}
-	txCreate := db.Create(collection)
-	if txCreate.Error != nil {
-		txUpdate := db.Save(collection)
-		if txUpdate.Error != nil {
-			panic(txUpdate.Error)
-		}
-	}
-
-	account := &entities.Account{ID: 0}
-	txCreate = db.Create(account)
-	if txCreate.Error != nil {
-		txUpdate := db.Save(account)
-		if txUpdate.Error != nil {
-			panic(txUpdate.Error)
-		}
-	}
 }
 
 func GetDB() *gorm.DB {
