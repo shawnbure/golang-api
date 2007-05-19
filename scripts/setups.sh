3a8a749885f1dcaf934e585be16653a1432e4c40
Newname <new@email>
2007-05-19 01:01:01 -0700

    refactor :topor:

--- scripts/setups.sh
+++ scripts/setups.sh
@@ -9,7 +9,9 @@ PSQL_ADDR_REPLACE="#listen_addresses = 'localhost'"
 PSQL_ADDR_WITH="listen_addresses = '$MACHINE_IP'"
 PSQL_CLIENT_CONNECT_OPT="host        all         all             0.0.0.0/0               md5"
 
-function install() {
+REDIS_PSW=some_pass
+
+postgres_install() {
   if ! [ -x "$(command psql -V)" ]; then
     echo "postgresql is not installed on your system. installing"
 
@@ -25,7 +27,7 @@ function install() {
   fi
 }
 
-function setup() {
+postgres_setup() {
   echo "creating database with user: '$PSQL_USER' and password: '$PSQL_PSW'"
 
   sudo -u postgres bash -c "psql -c \"create user '$PSQL_USER' with password '$PSQL_PSW';\""
@@ -38,4 +40,20 @@ function setup() {
   sudo bash -c "echo -e \"\n$PSQL_CLIENT_CONNECT_OPT\" >>/etc/postgresql/12/main/pg_hba.conf"
 
   sudo ufw allow from any to any port 5432 proto tcp
+}
+
+redis-install() {
+  sudo apt-get install redis-server
+
+  replace="bind 127.0.0.1 ::1"
+  with="bind $MACHINE_IP ::1"
+  sudo sed -i -e "s|$replace|$with|g" /etc/redis/redis.conf
+
+  replace="# requirepass foobared"
+  with="requirepass $REDIS_PSW"
+  sudo sed -i -e "s|$replace|$with|g" /etc/redis/redis.conf
+}
+
+redis-restart() {
+  sudo systemctl restart redis
 }
