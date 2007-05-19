d703e4a147f5cf65138522c6fc08cf9de3e32302
Newname <new@email>
2007-05-19 01:01:01 -0700

    rm garbage

--- storage/interface.go
+++ storage/interface.go
@@ -3,31 +3,3 @@ package storage
 type Storer interface {
 	GetConnection()
 }
-/*
-users
-    ID unique
-    Address
-
-nfts
-    ID unique
-    ID creator
-    ID owner
-    ID collection
-    tokenId
-    nonce
-    price
-    link
-
-transactions
-    ID unique
-    ID nft
-    ID seller
-    ID buyer
-    tx hash
-
-collections
-    ID unique
-    name
-    tokenID
-
-*/
