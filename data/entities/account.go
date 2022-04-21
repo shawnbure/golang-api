291de6d8858dd12f29e56c43d9f451e86c8398e8
Hien Ton <hienthatton@hotmail.com>
2022-04-21 15:04:32 -0700

    add RoleAccount to Account

--- data/entities/account.go
+++ data/entities/account.go
@@ -1,21 +1,25 @@
 package entities
 
 type Account struct {
-	ID               uint64 `gorm:"primaryKey" json:"id"`
-	Address          string `json:"address" gorm:"index:,unique"`
-	Name             string `json:"name" gorm:"default:random()::text;index:,unique"`
-	Description      string `json:"description"`
-	Website          string `json:"website"`
-	TwitterLink      string `json:"twitterLink"`
-	InstagramLink    string `json:"instagramLink"`
-	CreatedAt        uint64 `json:"createdAt"`
-	ProfileImageLink string `json:"profileImageLink"`
-	CoverImageLink   string `json:"coverImageLink"`
-	MintedCount      uint64 `json:"MintedCount" gorm:"default:0"`
-	MaxBatchMint     uint64 `json:"maxBatchMint" gorm:"default:10"`
-	MaxLifetimeMint  uint64 `json:"maxLifetimeMint" gorm:"default:10000"`
+	ID               uint64      `gorm:"primaryKey" json:"id"`
+	Address          string      `json:"address" gorm:"index:,unique"`
+	Name             string      `json:"name" gorm:"default:random()::text;index:,unique"`
+	Description      string      `json:"description"`
+	Website          string      `json:"website"`
+	TwitterLink      string      `json:"twitterLink"`
+	InstagramLink    string      `json:"instagramLink"`
+	CreatedAt        uint64      `json:"createdAt"`
+	ProfileImageLink string      `json:"profileImageLink"`
+	CoverImageLink   string      `json:"coverImageLink"`
+	MintedCount      uint64      `json:"MintedCount" gorm:"default:0"`
+	MaxBatchMint     uint64      `json:"maxBatchMint" gorm:"default:10"`
+	MaxLifetimeMint  uint64      `json:"maxLifetimeMint" gorm:"default:10000"`
+	Role             AccountRole `json:"role" gorm:"default:'RoleUser'"`
 }
 
-/*
-MaxBatchMint and MaxLifetimeMint
-*/
+type AccountRole string
+
+const (
+	RoleUser  AccountRole = "RoleUser"
+	RoleAdmin AccountRole = "RoleAdmin"
+)
