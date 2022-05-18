ca442be6a8a96f2f15776654bdac1f201694fc5c
Amir Nouri <anasvsfs@gmail.com>
2022-05-18 11:52:21 +0900

    proxy

--- proxier/proxier.go
+++ proxier/proxier.go
@@ -1,7 +1,11 @@
 package proxier
 
+import "time"
+
 var IPs []string
 var ipIndex = 0
+var lastIteration time.Time = time.Now()
+var numberOfCalls float64 = 0
 
 // var mx sync.Mutex
 
@@ -21,6 +25,13 @@ func ChangeIP() string {
 func GetCurrentIP() string {
 	if len(IPs) == 0 {
 		return ""
+	}
+	numberOfCalls++
+	rate := numberOfCalls / float64(time.Since(lastIteration).Minutes())
+	if rate > 30 {
+		numberOfCalls = 0
+		lastIteration = time.Now()
+		return ChangeIP()
 	}
 	return IPs[ipIndex]
 }
