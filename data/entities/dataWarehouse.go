5a48a5db4038a8dbff79734e8da45c9b880f322a
abolfazl behroozi <19313284+abolfazlbeh@users.noreply.github.com>
2022-05-20 21:09:40 +0430

    Aggregated Volume Per Collection Hourly (abolfazl.beh)
    - Aggregated volumes for 3 types "Buy", "List" and "Withdraw" per collection per hour are inserted to databases. for collections that data does not existed on database 0 will be inserted.
    - Some bugs fixing for stat page

--- data/entities/dataWarehouse.go
+++ data/entities/dataWarehouse.go
@@ -7,3 +7,19 @@ type AggregatedVolumePerHour struct {
 	ListVolume     float64 `json:"listVolume"`
 	WithdrawVolume float64 `json:"withdrawVolume"`
 }
+
+type AggregatedVolumePerCollectionPerHour struct {
+	ID           uint64 `gorm:"primaryKey" json:"id"`
+	Hour         int64  `json:"hour" gorm:"uniqueIndex:uidx_aggregated_collection_volume_per_hour"`
+	CollectionId uint64 `json:"collectionId" gorm:"uniqueIndex:uidx_aggregated_collection_volume_per_hour"`
+	//Collection     Collection `json:"collection"`
+	BuyVolume      float64 `json:"buyVolume"`
+	ListVolume     float64 `json:"listVolume"`
+	WithdrawVolume float64 `json:"withdrawVolume"`
+}
+
+type GroupAggregatedVolumePerCollection struct {
+	Total        float64 `json:"total"`
+	CollectionId uint64  `json:"collectionId"`
+	Type         string  `json:"type"`
+}
